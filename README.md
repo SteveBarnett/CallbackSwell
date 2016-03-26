bb3853ab4773c7e42e5e637e5f9d90ede8b05390
Steve Barnett <steve@naga.co.za>
2016-03-26 07:34:42 +0200

    Update examples to be shorter, clearer

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # CallbackSwell
 
-Quick examples of sync vs async.
+Async and callbacks can be tricky. Here's a quick exercise to help get your head around the ideas.
 
-Run `node index.js` and look at the output in the console.
+For more on callbacks and Node, look at [Chapter 20 of Eloquent JavaScript](http://eloquentjavascript.net/20_node.html) and [the Callbacks section of Art of Node](https://github.com/maxogden/art-of-node#callbacks).
 
-Things to think about:
-
-* What order will you see the `console.log` messages in?
-* Why does `productListReturn` return `undefined`, but `productListCallback` is fine?
+1. Write a short bit of code that uses the `fs` module to read a directory. The callback should look have `err` and `data` parameters. Use an if / else to catch the `err`. Run it using `node filename.js`.
+2. Now rewrite your code as a function with an argument.
+<!-- 3. Quick sidestep: pull out the callback as a named function. -->
+4. Use nested callbacks and `readFile` to read the contents of the dir.
--- index.js
+++ index.js
@@ -1,171 +1,88 @@
 var fs = require('fs');
 
-var list = 'list.txt';
-var listerr = 'listerr.txt';
-// var listForDir = 'list.txt';
-var listDir = 'lists';
+// 1. run it directly
 
-// sync
+fs.readdir('lists', function(err, data){
+// ^ hey, fs, read this dir.
+// when you're done, run `function(err, data)`
+    if (err) {
+      console.log('readdir err:', err);
+    } else { // no erors, yay!
+      // anything you want to do with `data` must happen inside here
+      // that's why callback hell happens: you start to nest callbacks
+      // (so that you can use the results)
+      console.log('readdir data:', data);
+    }
+});
 
-// var productsListSync = fs.readFileSync(list, 'utf8');
-// console.log('productsListSync:');
-// console.log(productsListSync);
-// console.log('---');
-//
-// // async, return it
-//
-// var productListReturn = fs.readFile(list, 'utf-8', function(err, data) {
-//   // console.log(data);
-//   return data;
-// });
-//
-// console.log('productListReturn:');
-// console.log(productListReturn);
-// console.log('---');
+var dirContents1 =  fs.readdir('lists', function(err, data){
+    if (err) {
+      console.log('dirContents1 err:', err);
+    } else {
+      console.log('dirContents1 data:', data);
+    }
+});
 
-// async, callback it
-
-// var productListCallback = fs.readFile(list, 'utf-8', function(err, data) {
-//   console.log('productListCallback:');
-//   console.log(data);
-//   console.log('---');
-// });
-//
-// async, catch errs and callback it
-
-// var productListCallbackAndCatchErrors = fs.readFile(list, 'utf-8', function(err, data) {
-//   console.log('productListCallbackAndCatchErrors:');
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-//   console.log('---');
-// });
-//
-// async, catch errs and callback it, with an error
-
-// var productListCallbackAndCatchErrorsWithError = fs.readFile(listerr, 'utf-8', function(err, data) {
-//   console.log('productListCallbackAndCatchErrorsWithError:');
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-//   console.log('---');
-// });
-//
-// async, catch errors, named callback
-
-// var productListNamedCallbackAndCatchErrors = fs.readFile(list, 'utf-8', function callback(err, data) {
-//   console.log('productListNamedCallbackAndCatchErrors:');
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-//   console.log('---');
-// });
-
-// async, catch errors, named external callback
-//
-// var productListNamedExternalCallback = fs.readFile(list, 'utf-8', qwerty);
-//
-// function qwerty(err, data) {
-//   console.log('productListNamedExternalCallback:');
-//   console.log(data);
-//   console.log('---');
-// }
-
-// var productListTwoCallBacks = fs.readdir(listDir, function(err, dirData) {
-//   // console.log("readdir result:");
-//   // console.log(dirData);
-//   // console.log('---');
-//
-//   dirData.forEach(function(file) {
-//     // console.log("readdir file:");
-//     // console.log(file);
-//     // console.log('---');
-//
-//     var currentFile = fs.readFile(listDir + '/' + file, 'utf8', function(err, fileData) {
-//       // console.log("fileData inside:" + file);
-//       // console.log(fileData);
-//       // console.log('---');
-//     });
-//
-//   //   console.log("currentFile outside the callback:");
-//   //   console.log(currentFile);
-//   //   console.log('---');
-//   });
-//
-// });
-
-// var productListTwoCallBacksReturn = fs.readdir(listDir, function(err, dirData) {
-//
-//   // dirData.forEach(function(file) {
-//     var currentFile = fs.readFile('lists/list1.txt', 'utf8', function(err, fileData) {
-//       return fileData;
-//     });
-//
-//
-//
-//   // });
-//
-// });
-
-var fs = require('fs');
-
-//
-//
-// function addOne(callback) {
-//   fs.readFile('numbers.txt', 'utf8', function doneReading(err, fileContents) {
-//     callback(fileContents);
-//   })
-// }
-//
-// function logMyNumber(chips) {
-//   console.log(chips);
-// }
-//
-// addOne(logMyNumber);
+// This will be undefined because it's not inside the callback:
+// by the time this `console.log` runs, `dirContents1` isn't set.
+console.log('dirContents1: ', dirContents1);
 
 // ---
+// 2. run it as a function, with arg
 
-function linesInFilesAsync(callback) {
-  fs.readFile('lists/list1.txt', 'utf8', function(err, data){
-    if (err) {
-      console.log(err);
-    } else {
-      callback(data);
-    }
+var linesInFilesAsync2 = function(folderName) {
+  fs.readdir(folderName, function(err, data){
+      if (err) {
+        console.log('linesInFilesAsync2 err: ', err);
+      } else {
+        console.log('linesInFilesAsync2 data: ', data);
+        // completion of the readdir `return`s `data`
+        // do stuff here, don't `return` it
+      }
   });
-}
+};
 
-function cbRadio(filestuff) {
-  console.log(filestuff);
-}
+// this is undefined because it tries to do stuff outside of the callback
+console.log("linesInFilesAsync2('lists'): ", linesInFilesAsync2('lists'));
 
-linesInFilesAsync(cbRadio);
-
-
-// function linesInFilesAsync(folderName, callback) {
-//   var fish = fs.readFile(folderName, 'utf8', function(err, data){
-//     // if (err) {
-//     //   console.log(err);
-//     // } else {
-//       // callback(data);
-//       // console.log(data);
-//       return(data);
-//     // }
-//   });
+// // 3. pull the callback out as a named function
+//
+// var linesInFilesAsync3 = function(folderName) {
+//   fs.readdir(folderName, logTheReaddir);
+// };
+//
+// function logTheReaddir(err, data){
+//     if (err) {
+//       console.log('linesInFilesAsync3 err: ', err);
+//     } else {
+//       console.log('linesInFilesAsync3 data: ', data);
+//     }
 // }
 //
-// // function cbRadio(filestuff) {
-// //   console.log(filestuff);
-// // }
-//
-// // linesInFilesAsync(cbRadio);
-//
-// linesInFilesAsync('lists/list1.txt', function(err, data){
-//   console.log(fish);
-// });
+// linesInFilesAsync3('lists');
+
+// 4. run something inside it
+
+var linesInFilesAsync4 = function(folderName) {
+  fs.readdir(folderName, function(err, data){
+      if (err) {
+        console.log('linesInFilesAsync4 readdir err: ', err);
+      } else {
+        console.log('linesInFilesAsync4 readdir data: ', data);
+        data.forEach(function(files){
+          console.log('linesInFilesAsync4 readdir forEach file: ', files);
+          fs.readFile(folderName + '/' + files, 'utf8', function(errRF, dataRF) {
+            if (errRF) {
+              console.log('linesInFilesAsync4 readFile err: ', errRF);
+            } else {
+              console.log('linesInFilesAsync4 readFile data: ', dataRF);
+              // if you want to do something with the file contents,
+              // it has to happen here
+            }
+          });
+        });
+      }
+  });
+};
+
+linesInFilesAsync4('lists');
